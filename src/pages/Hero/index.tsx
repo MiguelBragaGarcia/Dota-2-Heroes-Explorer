18f39fa563e951bd4e405144e1781495e6430c70
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-07-08 19:06:33 -0300

    Iniciando a página Hero

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -1,7 +1,169 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import api from '../../services/api';
+
+import {
+  Container,
+  HeroContainer,
+  HeroHeader,
+  HeroFunction,
+  AttributeContainer,
+  Attribute,
+  HeroInfoContainer,
+  HeroTextInfo,
+} from './styles';
+
+interface HeroParams {
+  id: string;
+}
+
+interface HeroData {
+  id: number;
+  localized_name: string;
+  primary_attr: string;
+  img: string;
+  base_str: number;
+  base_agi: number;
+  base_int: number;
+  base_armor: number;
+  attack_type: string;
+  roles: string[];
+  move_speed: number;
+
+  base_health: number;
+  base_mana: number;
+
+  base_attack_min: number;
+  base_attack_max: number;
+
+  str_gain: number;
+  agi_gain: number;
+  int_gain: number;
+}
 
 const Hero: React.FC = () => {
-  return <h1>Página de vídeo</h1>;
+  const [selectedHero, setSelectedHero] = useState<HeroData>();
+  const manaPerIntelligence = 12;
+  const healthPerIntelligence = 20;
+  const agilityPerArmor = 6;
+  const { params } = useRouteMatch<HeroParams>();
+
+  const heroId = Number(params.id);
+  useEffect(() => {
+    async function loadHeroesData() {
+      const cdnSteam = 'https://steamcdn-a.akamaihd.net';
+
+      const response = await api.get<HeroData[]>('/heroStats');
+      const formattedHeroes = response.data.map((hero) => {
+        return {
+          ...hero,
+          img: cdnSteam.concat(hero.img),
+        };
+      });
+
+      const filteredHero = formattedHeroes.find((hero) => hero.id === heroId);
+
+      setSelectedHero(filteredHero);
+    }
+
+    loadHeroesData();
+  }, [heroId]);
+
+  const CalculatedMana = useMemo(() => {
+    if (selectedHero) {
+      return (
+        selectedHero.base_mana + selectedHero.base_int * manaPerIntelligence
+      );
+    }
+  }, [selectedHero]);
+
+  const CalculatedHealth = useMemo(() => {
+    if (selectedHero) {
+      return (
+        selectedHero.base_health + selectedHero.base_str * healthPerIntelligence
+      );
+    }
+  }, [selectedHero]);
+
+  const CalculatedArmor = useMemo(() => {
+    if (selectedHero) {
+      const armor =
+        selectedHero.base_armor + selectedHero.base_agi / agilityPerArmor;
+
+      return armor.toFixed(2);
+    }
+  }, [selectedHero]);
+
+  const CalculatedMinAttack = useMemo(() => {
+    if (selectedHero) {
+      return selectedHero.base_attack_min + selectedHero.base_agi;
+    }
+  }, [selectedHero]);
+
+  const CalculatedMaxAttack = useMemo(() => {
+    if (selectedHero) {
+      return selectedHero.base_attack_max + selectedHero.base_agi;
+    }
+  }, [selectedHero]);
+
+  return (
+    <Container>
+      {selectedHero && (
+        <HeroContainer>
+          <img src={selectedHero.img} alt={selectedHero.localized_name} />
+          <HeroInfoContainer>
+            <HeroHeader>
+              <strong>{selectedHero.localized_name}</strong>
+
+              <p>{selectedHero.attack_type}</p>
+            </HeroHeader>
+            <HeroFunction>
+              {selectedHero.roles.map((role) => (
+                <p>{role}</p>
+              ))}
+            </HeroFunction>
+
+            <AttributeContainer>
+              <Attribute type="str">
+                {selectedHero.base_str} + {selectedHero.str_gain}
+              </Attribute>
+
+              <Attribute type="agi">
+                {selectedHero.base_agi} + {selectedHero.agi_gain}
+              </Attribute>
+
+              <Attribute type="int">
+                {selectedHero.base_int} + {selectedHero.int_gain}
+              </Attribute>
+            </AttributeContainer>
+
+            <HeroTextInfo>
+              <p>Vida</p>
+              <p>{CalculatedHealth}</p>
+            </HeroTextInfo>
+            <HeroTextInfo>
+              <p>Mana</p>
+              <p>{CalculatedMana}</p>
+            </HeroTextInfo>
+            <HeroTextInfo>
+              <p>Velocidade de movimento </p>
+              <p>{selectedHero.move_speed}</p>
+            </HeroTextInfo>
+            <HeroTextInfo>
+              <p>Ataque</p>{' '}
+              <p>
+                {CalculatedMinAttack} ~ {CalculatedMaxAttack}
+              </p>
+            </HeroTextInfo>
+
+            <HeroTextInfo>
+              <p>Armadura</p> <p>{CalculatedArmor}</p>
+            </HeroTextInfo>
+          </HeroInfoContainer>
+        </HeroContainer>
+      )}
+    </Container>
+  );
 };
 
 export default Hero;
--- src/pages/Hero/styles.ts
+++ src/pages/Hero/styles.ts
@@ -1,3 +1,131 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const container = styled.div``;
+interface AttributeInfo {
+  type: string;
+}
+
+export const Container = styled.div`
+  max-width: 1280px;
+  margin: 80px auto;
+  padding: 10px;
+`;
+
+export const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 15px;
+  background: #0f1211;
+  color: #fff;
+
+  strong {
+    font-size: 26px;
+  }
+
+  img {
+    border-radius: 5px;
+    margin-left: 5%;
+    margin-top: 40px;
+    width: 350px;
+    height: 170px;
+  }
+`;
+
+export const HeroHeader = styled.div`
+  display: inline-flex;
+  align-items: baseline;
+  margin-bottom: 10px;
+
+  p {
+    margin-left: 15px;
+    font-weight: bold;
+  }
+`;
+
+export const HeroFunction = styled.div`
+  display: flex;
+  p {
+    font-weight: bold;
+    font-size: 14px;
+    & + p {
+      margin: 0 0 0 40px;
+    }
+  }
+`;
+
+export const AttributeContainer = styled.div`
+  display: flex;
+  margin: 10px 20px 0;
+`;
+
+export const Attribute = styled.p<AttributeInfo>`
+  display: flex;
+  font-weight: 400;
+  padding: 3px 5px;
+  font-size: 15px;
+
+  &+p {
+    margin-left: 20px;
+  }
+
+    ${(props) =>
+      props.type === 'str' &&
+      css`
+        color: red;
+
+        &::before {
+          content: '';
+          height: 15px;
+          width: 15px;
+          background-color: red;
+          border-radius: 50%;
+          display: inline-block;
+          margin-right: 5px;
+        }
+      `}
+    ${(props) =>
+      props.type === 'agi' &&
+      css`
+        color: green;
+
+        &::before {
+          content: '';
+          height: 15px;
+          width: 15px;
+          background-color: green;
+          border-radius: 50%;
+          display: inline-block;
+          margin-right: 5px;
+        }
+      `}
+    ${(props) =>
+      props.type === 'int' &&
+      css`
+        color: blue;
+
+        &::before {
+          content: '';
+          height: 15px;
+          width: 15px;
+          background-color: blue;
+          border-radius: 50%;
+          display: inline-block;
+          margin-right: 5px;
+        }
+      `};
+`;
+
+export const HeroInfoContainer = styled.div`
+  margin: 0 30px;
+  width: 100%;
+  border: 1px solid #0f1211;
+`;
+
+export const HeroTextInfo = styled.div`
+  display: flex;
+  width: 45%;
+  justify-content: space-between;
+
+  font-size: 14px;
+  font-weight: bold;
+  margin: 10px 0;
+`;
