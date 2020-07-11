3fe9f426639983b86376583eaf188a4951137775
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-07-11 10:27:14 -0300

    Adicionando paginação e header, corrigindo erro de calculo de ataque

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/dota-logo.svg';
 import api from '../../services/api';
 
 import {
   Container,
+  HeaderContainer,
+  Header,
+  List,
+  ContentContainer,
   HeroContainer,
   HeroHeader,
   HeroFunction,
@@ -13,6 +20,7 @@ import {
   HeroTextInfo,
   LastMatchsWithTheHero,
   Match,
+  PageAction,
 } from './styles';
 
 interface HeroParams {
@@ -55,15 +63,25 @@ interface ProMatches {
 }
 
 const Hero: React.FC = () => {
+  const [totalProMatches, setTotalProMatches] = useState(0);
+  const [matchesInPage, setMatchesInPage] = useState<ProMatches[]>();
+  const [page, setPage] = useState(1);
+  const [nextPage, setNextPage] = useState(true);
+  const [prevPage, setPrevPage] = useState(true);
+
   const [selectedHero, setSelectedHero] = useState<HeroData>();
   const [matches, setMatches] = useState<ProMatches[] | undefined>();
 
   const manaPerIntelligence = 12;
   const healthPerIntelligence = 20;
   const agilityPerArmor = 6;
+
+  const matchesPerPage = 5;
+
   const { params } = useRouteMatch<HeroParams>();
 
   const heroId = Number(params.id);
+
   useEffect(() => {
     async function loadHeroesData() {
       const cdnSteam = 'https://steamcdn-a.akamaihd.net';
@@ -121,11 +139,32 @@ const Hero: React.FC = () => {
         };
       });
 
+      setTotalProMatches(formattedProMatches.length);
+
       setMatches(formattedProMatches);
     }
 
     loadProMatches();
   }, [heroId]);
+
+  useEffect(() => {
+    if (matches) {
+      const nextPageEnabled = page < totalProMatches / matchesPerPage;
+      const prevPageEnabled = page > 1;
+
+      const proMatchesResult = matches.filter((match, index) => {
+        return (
+          index >= (page - 1) * matchesPerPage &&
+          index <= page * matchesPerPage - 1
+        );
+      });
+
+      setMatchesInPage(proMatchesResult);
+
+      setPrevPage(prevPageEnabled);
+      setNextPage(nextPageEnabled);
+    }
+  }, [matches, page, totalProMatches]);
 
   const CalculatedMana = useMemo(() => {
     if (selectedHero) {
@@ -154,92 +193,155 @@ const Hero: React.FC = () => {
 
   const CalculatedMinAttack = useMemo(() => {
     if (selectedHero) {
-      return selectedHero.base_attack_min + selectedHero.base_agi;
+      if (selectedHero.primary_attr === 'str') {
+        return selectedHero.base_attack_min + selectedHero.base_str;
+      }
+      if (selectedHero.primary_attr === 'agi') {
+        return selectedHero.base_attack_min + selectedHero.base_agi;
+      }
+
+      return selectedHero.base_attack_min + selectedHero.base_int;
     }
   }, [selectedHero]);
 
   const CalculatedMaxAttack = useMemo(() => {
     if (selectedHero) {
-      return selectedHero.base_attack_max + selectedHero.base_agi;
+      if (selectedHero.primary_attr === 'str') {
+        return selectedHero.base_attack_max + selectedHero.base_str;
+      }
+      if (selectedHero.primary_attr === 'agi') {
+        return selectedHero.base_attack_max + selectedHero.base_agi;
+      }
+
+      return selectedHero.base_attack_max + selectedHero.base_int;
     }
   }, [selectedHero]);
 
   return (
     <Container>
-      {selectedHero && (
-        <HeroContainer key={selectedHero.id}>
-          <img src={selectedHero.img} alt={selectedHero.localized_name} />
-          <HeroInfoContainer>
-            <HeroHeader>
-              <strong>{selectedHero.localized_name}</strong>
+      <HeaderContainer>
+        <Header>
+          <Link to="/">
+            <img src={logo} alt="dota" />
+          </Link>
 
-              <p>{selectedHero.attack_type}</p>
-            </HeroHeader>
-            <HeroFunction>
-              {selectedHero.roles.map((role) => (
-                <p key={role}>{role}</p>
-              ))}
-            </HeroFunction>
+          <List>
+            <li>
+              <button type="button">Inicio</button>
+            </li>
+            <li>
+              <button type="button">Comunidades</button>
+            </li>
+            <li>
+              <button type="button">Vídeos</button>
+            </li>
+            <li>
+              <button type="button">Pesquisar</button>
+            </li>
+          </List>
+        </Header>
+      </HeaderContainer>
 
-            <AttributeContainer>
-              <Attribute type="str">
-                {selectedHero.base_str} + {selectedHero.str_gain}
-              </Attribute>
+      <ContentContainer>
+        {selectedHero && (
+          <HeroContainer key={selectedHero.id}>
+            <img src={selectedHero.img} alt={selectedHero.localized_name} />
+            <HeroInfoContainer>
+              <HeroHeader>
+                <strong>{selectedHero.localized_name}</strong>
 
-              <Attribute type="agi">
-                {selectedHero.base_agi} + {selectedHero.agi_gain}
-              </Attribute>
+                <p>{selectedHero.attack_type}</p>
+              </HeroHeader>
+              <HeroFunction>
+                {selectedHero.roles.map((role) => (
+                  <p key={role}>{role}</p>
+                ))}
+              </HeroFunction>
 
-              <Attribute type="int">
-                {selectedHero.base_int} + {selectedHero.int_gain}
-              </Attribute>
-            </AttributeContainer>
+              <AttributeContainer>
+                <Attribute type="str">
+                  {selectedHero.base_str} + {selectedHero.str_gain}
+                </Attribute>
 
-            <HeroTextInfo>
-              <p>Vida</p>
-              <p>{CalculatedHealth}</p>
-            </HeroTextInfo>
-            <HeroTextInfo>
-              <p>Mana</p>
-              <p>{CalculatedMana}</p>
-            </HeroTextInfo>
-            <HeroTextInfo>
-              <p>Velocidade de movimento </p>
-              <p>{selectedHero.move_speed}</p>
-            </HeroTextInfo>
-            <HeroTextInfo>
-              <p>Ataque</p>{' '}
-              <p>
-                {CalculatedMinAttack} ~ {CalculatedMaxAttack}
-              </p>
-            </HeroTextInfo>
+                <Attribute type="agi">
+                  {selectedHero.base_agi} + {selectedHero.agi_gain}
+                </Attribute>
 
-            <HeroTextInfo>
-              <p>Armadura</p> <p>{CalculatedArmor}</p>
-            </HeroTextInfo>
-          </HeroInfoContainer>
-        </HeroContainer>
-      )}
-      <LastMatchsWithTheHero>
-        <p>Últimas partidas profissionais com este herói</p>
+                <Attribute type="int">
+                  {selectedHero.base_int} + {selectedHero.int_gain}
+                </Attribute>
+              </AttributeContainer>
 
-        {matches &&
-          matches.map((match) => (
-            <a href={match.match_link} key={match.match_id}>
-              <Match>
-                <img
-                  src={selectedHero?.icon}
-                  alt={selectedHero?.localized_name}
-                />
-                <strong>{match.league_name}</strong>
+              <HeroTextInfo>
+                <p>Vida</p>
+                <p>{CalculatedHealth}</p>
+              </HeroTextInfo>
+              <HeroTextInfo>
+                <p>Mana</p>
+                <p>{CalculatedMana}</p>
+              </HeroTextInfo>
+              <HeroTextInfo>
+                <p>Velocidade de movimento </p>
+                <p>{selectedHero.move_speed}</p>
+              </HeroTextInfo>
+              <HeroTextInfo>
+                <p>Ataque</p>{' '}
+                <p>
+                  {CalculatedMinAttack} ~ {CalculatedMaxAttack}
+                </p>
+              </HeroTextInfo>
 
-                <strong>
-                  {match.kills}/{match.deaths}/{match.assists}
-                </strong>
-              </Match>
-            </a>
-          ))}
-      </LastMatchsWithTheHero>
+              <HeroTextInfo>
+                <p>Armadura</p> <p>{CalculatedArmor}</p>
+              </HeroTextInfo>
+            </HeroInfoContainer>
+          </HeroContainer>
+        )}
+        <LastMatchsWithTheHero>
+          <p>Últimas partidas profissionais com este herói</p>
+
+          {matchesInPage &&
+            matchesInPage.map((match) => (
+              <a href={match.match_link} key={match.match_id}>
+                <Match>
+                  <img
+                    src={selectedHero?.icon}
+                    alt={selectedHero?.localized_name}
+                  />
+                  <strong>{match.league_name}</strong>
+
+                  <strong>
+                    {match.kills}/{match.deaths}/{match.assists}
+                  </strong>
+                </Match>
+              </a>
+            ))}
+
+          <PageAction>
+            <button
+              type="button"
+              disabled={!prevPage}
+              onClick={() => setPage(page - 1)}
+            >
+              <FiChevronLeft
+                size={30}
+                color={!prevPage ? '#363636' : '#cfcfcf'}
+              />
+            </button>
+            <span>{page}</span>
+            <button
+              type="button"
+              disabled={!nextPage}
+              onClick={() => setPage(page + 1)}
+            >
+              <FiChevronRight
+                size={30}
+                color={!nextPage ? '#363636' : '#cfcfcf'}
+              />
+            </button>
+          </PageAction>
+        </LastMatchsWithTheHero>
+      </ContentContainer>
     </Container>
   );
 };
